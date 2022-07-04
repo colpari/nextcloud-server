ab09d1dfd5d59b7ba4596623d88dadd791066700
Anna Larch <anna@nextcloud.com>
2022-07-04 15:52:24 +0200

    fixup! Add logging to federation

--- apps/dav/lib/CardDAV/SyncService.php
+++ apps/dav/lib/CardDAV/SyncService.php
@@ -74,6 +74,7 @@ class SyncService {
 				// remote server revoked access to the address book, remove it
 				$this->backend->deleteAddressBook($addressBookId);
 				$this->logger->error('Authorization failed, remove address book: ' . $url, ['app' => 'dav']);
+				throw $ex;
 			}
 			$this->logger->error('Client exception:', ['app' => 'dav', 'exception' => $ex]);
 			throw $ex;
